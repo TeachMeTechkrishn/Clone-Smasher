320cd5861cb9640e3cd60d1b20a096b8cd15f9e6
TeachMeTechkrishn <krishnatiwari3189311520@gmail.com>
2024-10-29 21:47:53 +0530

    Update config.js
    Noob

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 export default {
 	"botNames": [
-		"Clone Smasher"
+		"Noob"
 	],
 	"accounts": [
 		""
 	],
 	"proxyType": "socks4", // Possible types: socks4, socks5, http (defaults to socks4 if invalid input)
 	"useProxyApi": true,
-	"useAccount": false,
-	"maxBots": 100
+	"useAccount": true,
+	"maxBots": 300
 }
